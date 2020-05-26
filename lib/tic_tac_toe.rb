75b77780959cb332963943617180fb4920e97e11
Jasmine Lok <jasminellok@gmail.com>
2020-05-26 22:27:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -99,3 +99,14 @@ def full?(board)
     return true
   end
 end
+
+
+def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif !full?(board) && !won?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
